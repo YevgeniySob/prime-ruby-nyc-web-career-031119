b809d6a9a2273d47dca9c2c1f415e8f673f4f8ee
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-19 00:10:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  elsif num == 2 || num == 3
+    return true
+  end
+  for i in 2..num
+    if num%i == 0
+      return false
+    else
+      puts num
+      puts i
+      puts "hey"
+      puts num/i
+      return true
+    end
+  end
+end
+
+prime?(39)
